9de1b482dc4d3d799cef8da3dd623f11d4c0b774
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-22 13:27:06 -0800

    testing Driver methods

--- src/main/CarAndDriverTest.java
+++ src/main/CarAndDriverTest.java
@@ -5,7 +5,14 @@ public class CarAndDriverTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Driver driver1 = new Driver();
-		driver1.driveCar();
+		driver1.drive();
+		driver1.drive();
+		driver1.drive();
+		driver1.drive();
+		driver1.useBoosters();
+		driver1.refuel();
+		driver1.refuel();
+		driver1.refuel();
 		
 
 	}
--- src/main/Driver.java
+++ src/main/Driver.java
@@ -4,9 +4,29 @@ public class Driver extends Car{
 	
 	public Driver() {}
 	
-	public void driveCar() {
+	public void drive() {
+		System.out.println("\n --- DRIVING CAR ---");
 		Integer newGasAmount = super.getGasAmount() - 1;
 		super.setGasAmount(newGasAmount);
 		super.status();
+		System.out.println("------------------");
 	}
+	
+	public void useBoosters() {
+		System.out.println("\n --- USING BOOSTERS ---");
+		Integer newGasAmount = super.getGasAmount() - 3;
+		super.setGasAmount(newGasAmount);
+		super.status();
+		System.out.println("------------------");
+	}
+	
+	public void refuel() {
+		System.out.println("\n --- REFUELING CAR ---");
+		Integer newGasAmount = super.getGasAmount() + 2;
+		super.setGasAmount(newGasAmount);
+		super.status();
+		System.out.println("------------------");
+	}
+	
+	
 }
